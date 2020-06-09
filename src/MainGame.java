da605dcc98b8925acfb3a3b3244372f7bb90c737
bcastanerc <60187468+bcastanerc@users.noreply.github.com>
2020-06-09 17:49:28 +0200

    Juego finalizado.

--- src/MainGame.java
+++ src/MainGame.java
@@ -6,8 +6,6 @@ import org.newdawn.slick.geom.Circle;
 import org.newdawn.slick.geom.Rectangle;
 import org.newdawn.slick.geom.Shape;
 
-import java.awt.*;
-
 public class MainGame {
     static final int WIDTH = 1024;
     static final int HEIGHT = 576;
@@ -18,12 +16,14 @@ public class MainGame {
        AppGameContainer ap = new AppGameContainer(CaGame, WIDTH, HEIGHT, SCREEN);
        ap.setTargetFrameRate(59);
        ap.start();
-
     }
 }
 
+/**
+ * Esta es la clase principal donde se encuentra el cuerpo del juego.
+ */
 class CannonGame extends BasicGame{
-   // private Font fuente;
+
     Font gameFont;
     Font menuFont;
     private Cannon cannon;
@@ -31,13 +31,20 @@ class CannonGame extends BasicGame{
     private Target target;
     private Landscape landscape;
     private int score = 0;
+    // Vatiable para saber el estado del juego.
     private boolean gameStarted = false;
     private Image startBG;
+    private int consecutiveHits = 0;
 
     public CannonGame(String title) throws SlickException {
         super(title);
     }
 
+    /**
+     * En esta clase se inicializan los elementos del juego.
+     * @param gameContainer contenerdor del CannonGame.
+     * @throws SlickException excepcion de la libreria que estamos usando.
+     */
     @Override
     public void init(GameContainer gameContainer) throws SlickException {
 
@@ -49,6 +56,12 @@ class CannonGame extends BasicGame{
         target = new Target();
     }
 
+    /**
+     * En esta cl
+     * @param gameContainer contenerdor del CannonGame.
+     * @param i  parametro interno de la libreria.
+     * @throws SlickException excepcion de la libreria que estamos usando.
+     */
     @Override
     public void update(GameContainer gameContainer, int i) throws SlickException {
         Input input = gameContainer.getInput();
@@ -77,10 +90,9 @@ class CannonGame extends BasicGame{
             // Cuando se pulsa el espacio dispara la pelota.
             if (input.isKeyPressed(Input.KEY_SPACE) && this.cannon.getShootsRemaining() > 0 && gameStarted && this.ball==null){
                ball = this.cannon.fire();
-                // Le da una nueva posición al Target
             }
 
-            //
+            // La Pelota por defecto es null hasta que no se pulsa el espacio, una vez se ha disparado se crea una.
             if (ball != null){
                 ball.update();
                 if (this.ball.fallen()){
@@ -95,26 +107,38 @@ class CannonGame extends BasicGame{
 
     }
 
+    /**
+     * En esta clase se hace el apartado grafico del juego.
+     * @param gameContainer contenerdor del CannonGame.
+     * @param graphics  parámetro interno de la libreria.
+     * @throws SlickException excepcion de la libreria que estamos usando.
+     */
     @Override
     public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
         if (!gameStarted){
             startBG.draw(0,0);
             this.menuFont.drawString(270,280,"Press SPACE to continue.");
         }else{
-
-            // Si la pelota se ha disparado y ha caido resetea la pelota.
             this.landscape.render();
             this.target.render();
+
+            // Si la pelota se ha disparado y ha caido resetea la pelota.
             if (ball != null){
                 this.ball.render();
                 if (target.hit(ball.getCircle())) {
                     this.ball=null;
+                    consecutiveHits++;
+                    // Al dar 3 golpes consecutivos da 1 disparo más
+                    if (consecutiveHits == 3){
+                        consecutiveHits = 0;
+                        this.cannon.setShootsRemaining(this.cannon.getShootsRemaining()+1);
+                    }
                     score+=100;
+                    this.gameFont.drawString(this.target.getPosX()+55,this.target.getPosY()-40,"HIT");
                     this.target.reset();
                 }
             }
             this.cannon.render();
-            graphics.draw(target.getShape());
             this.gameFont.drawString(100,7,"Angle: " + Math.abs(this.cannon.getCannonRotation()));
             this.gameFont.drawString(350,7,"Strenght: " + this.cannon.getCannonStrenght());
             this.gameFont.drawString(600,7,"Shoots: " + this.cannon.getShootsRemaining());
@@ -124,13 +148,21 @@ class CannonGame extends BasicGame{
     }
 }
 
+/**
+ * Clase cannon la cual simula el cañon del juego.
+ */
 class Cannon {
+
     private Image cannonBase;
     private Image cannon;
     private float rotation;
     private double cannonStrenght;
     private int shootsRemaining = 5;
 
+    /**
+     * Constructor del cañon.
+     * @throws SlickException
+     */
     Cannon() throws SlickException {
         cannonBase = new Image("resources\\cannon_base.png");
         cannon = new Image("resources\\cannon.png");
@@ -140,11 +172,11 @@ class Cannon {
         cannonStrenght = 50;
     }
 
-    public void update(GameContainer gameContainer){
-
-    }
-
+    /**
+     * Parte gráfica del caón.
+     */
     public void render(){
+
         // Para cuadrar el cañon lo desplazamos 10px a la izquierda y 40 arriba.
         int cannonBaseX = 40;
         int cannonBaseY = 500;
@@ -155,6 +187,11 @@ class Cannon {
         cannonBase.draw(cannonBaseX, cannonBaseY);
     }
 
+    /**
+     * Metodo de cannon el cual disàra la pelota.
+     * @return devuelve una nueva pelota con sus caracteristicas.
+     * @throws SlickException
+     */
     public Ball fire() throws SlickException {
        return new Ball(Math.abs(this.getCannonRotation()),this.getCannonStrenght());
     }
@@ -181,16 +218,25 @@ class Cannon {
     }
 }
 
+/**
+ * Esta clase define el fondo del juego.
+ */
 class Landscape{
+
     private Image background;
     private Image cloud;
     private int cloudY = 100;
     private int cloudX = 0;
     private boolean movingBack = false;
+
     Landscape() throws SlickException {
         background = new Image("resources\\landscape.jpg");
         cloud = new Image("resources\\cloud.png");
     }
+
+    /**
+     * Este método actualiza la posición de la nuve del fondo.
+     */
     public void update(){
         // Movimmiento de la nuve.
         if (cloudX == (background.getWidth() -cloud.getWidth())) movingBack = true;
@@ -198,6 +244,10 @@ class Landscape{
         if(!movingBack) cloudX++;
         else cloudX--;
     }
+
+    /**
+     * Este método pinta la la nuve y el fondo en el juego.
+     */
     public void render(){
         background.draw(0,0);
         cloud.draw(cloudX,cloudY);
@@ -219,11 +269,19 @@ class Landscape{
     }
 }
 
+/**
+ * Clase que define al objetivo del juego.
+ */
 class Target{
     private Image target;
     private Shape rectangle;
     private int posX;
     private int posY = 500;
+
+    /**
+     * Por defecto le da una posición aleatoria al Target cada vez que se crea
+     * @throws SlickException excepcion de la libreria que estamos usando.
+     */
     Target() throws SlickException {
         target = new Image("resources\\target.png");
         posX = (int)(Math.random() * 512) + (512 - target.getWidth());
@@ -237,9 +295,18 @@ class Target{
         target.draw(posX,posY);
     }
 
+    /**
+     *
+     * @param shape grosor del target en pixeles
+     * @return devuelve true si la pelota coincide con los pixeles de la posición del target y false no coincide.
+     */
     public boolean hit(Shape shape){
         return rectangle.intersects(shape);
     }
+
+    /**
+     * Este método le da una nueva posición al objetivo.
+     */
     public void reset(){
         posX = (int)(Math.random() * 512) + (512 - target.getWidth());
         rectangle.setX(posX);
@@ -278,6 +345,9 @@ class Target{
     }
 }
 
+/**
+ * Clase que representa a la pelota en el juego.
+ */
 class Ball{
     private Image ball = new Image("resources\\ball.png");
     private Shape circle;
@@ -286,7 +356,6 @@ class Ball{
     private int ballRotation = 0;
     private float pX = 0;
     private float pY= 0;
-    // La fuerza de la gravedad inferior a la de la tierra.
     private static final double GRAVITY = 9.8;
     private float time = 0;
     private float vx;
@@ -294,8 +363,7 @@ class Ball{
 
     Ball(double rot, double str) throws SlickException {
         ball.setCenterOfRotation(ball.getWidth()/2,ball.getHeight()/2);
-        System.out.println("Fuerza:" + str + "Rotación:" + rot);
-        // Multiplica la velocidad para que no haya que usar tanta fuerza en el cañon,le da un 30% más de fuera al disparo.
+        // Multiplica la velocidad para que no haya que usar tanta fuerza en el cañon,le da un 20% más de fuerza al disparo.
         float vel = (float) ((float) str * 1.2);
         float angle = (float) (rot * Math.PI / 180f);
         vx = (float) (vel * Math.cos(angle));
@@ -317,22 +385,31 @@ class Ball{
         this.circle = circle;
     }
 
+    /**
+     * Calcula si la pelota se ha caido de la pantalla. Al superar los 580px (height del juego) determina que ha caido.
+     * @return devuelve true si ha caido, si no false.
+     */
     public boolean fallen(){
         return pY >= 580;
     }
 
+    /**
+     * El método update de la clase Ball simula la parabola gracias a la gravedad, angulo y fuerza del cañon.
+     */
     public void update(){
-
         pX = posX +vx*time;
         pY = (float) (posY +vy*time + GRAVITY*time*time/2f);
-        System.out.printf("Tiempo: %.1f Posición: (PosX: %.1f || PosY: %.1f)\n",time,pX,pY);
         time+= 0.15f;
         circle = new Circle(pX,pY,20);
         circle.setCenterX(pX+20);
         circle.setCenterY(pY+20);
 
     }
-    public void render() throws SlickException {
+
+    /**
+     * Este método pinta la pelota en el juego.
+     */
+    public void render(){
         ball.draw(pX,pY);
         ball.setRotation(ballRotation = ballRotation+5);
     }
